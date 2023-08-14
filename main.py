7e718413b1fa445d3be509553c108c33fb746886
Eyad Hussein <eyadhussein952018@gmail.com>
2023-08-15 00:11:10 +0300

    v2

--- main.py
+++ main.py
@@ -1,93 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 13 02:20:31 2020
-
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
-
 import numpy as np
 import pickle
 import pandas as pd
-#from flasgger import Swagger
-import streamlit as st 
+import streamlit as st
 
 from PIL import Image
 
-#app=Flask(__name__)
-#Swagger(app)
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
 
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
 
-#@app.route('/')
 def welcome():
     return "Welcome All"
 
-#@app.route('/predict',methods=["Get"])
-def predict_note_authentication(variance,skewness,curtosis,entropy):
-    
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-   
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
-    print(prediction)
-    return prediction
 
+def predict_note_authentication(uploaded_file=None):
+    # if opt1 == "":
+    df = pd.read_csv(uploaded_file)
+    X = df[["registered", "casual"]]
+    output = classifier.predict(X)
+    output_df = pd.DataFrame({'cnt': output})
+    output_df.to_csv('linear_regression_output.csv', index=False)
+    # else:
+    # code for predicting single input
+    return output
 
 
 def main():
-    st.title("Bank Authenticator")
+    st.title("Project Deployment")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    <h2 style="color:white;text-align:center;">Project Deployment ITI</h2>
     </div>
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
-    curtosis = st.text_input("curtosis","Type Here")
-    entropy = st.text_input("entropy","Type Here")
-    result=""
+    st.markdown(html_temp, unsafe_allow_html=True)
+    result = ""
+    # for only one input
+    opt1 = st.text_input("Enter your input", "")
+    # importing dataset for test
+    uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
     if st.button("Predict"):
-        result=predict_note_authentication(variance,skewness,curtosis,entropy)
-    st.success('The output is {}'.format(result))
+        if (uploaded_file is not None and opt1 != ""):
+            st.text("Please choose only one option")
+        elif (uploaded_file is not None):
+            result = predict_note_authentication(uploaded_file)
+        else:
+            result = predict_note_authentication(opt1)
+    with st.expander("See Predictions"):
+        for i, item in enumerate(result):
+            st.write(i+1, " {}".format(round(item, 2)))
+        st.success('success')
+
     if st.button("About"):
-        st.text("Lets LEarn")
+        st.text("Lets Learn")
         st.text("Built with Streamlit")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-    
-    
-    
