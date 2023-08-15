10017322d105fd535f6b378bfc9d0c4397eb6ba5
Eyad Hussein <eyadhussein952018@gmail.com>
2023-08-16 01:35:21 +0300

    v1

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ import pandas as pd
 import streamlit as st
 import csv
 
-from PIL import Image
-
+# importing model from pickle file
 pickle_in = open("classifier.pkl", "rb")
 classifier = pickle.load(pickle_in)
 
-
-def welcome():
-    return "Welcome All"
+# predict whether the body mass is hazardous or not
 
 
-def predict_note_authentication(uploaded_file=None):
-    # if opt1 == "":
+def predict_danger_csv(uploaded_file):
     df = pd.read_csv(uploaded_file)
     X = df[["registered", "casual"]]
     output = classifier.predict(X)
-    output_df = pd.DataFrame({'cnt': output})
-    output_df.to_csv('linear_regression_output.csv', index=False)
-    # else:
-    # code for predicting single input
+    return output
+
+
+def predict_danger_single(arr):
+    X = np.array([arr])
+    output = classifier.predict(X)
     return output
 
 
@@ -41,40 +39,45 @@ def save_data_to_csv(item):
 
 
 def main():
+    # initial markup and initiation of important flags
     flag = 0
-    st.title("Project Deployment")
+
+    st.title("Outer Space")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Project Deployment ITI</h2>
+    <h2 style="color:white;text-align:center;">Is Death Upon Us?</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
     result = ""
+
     # for only one input
-    opt1 = st.text_input("Enter your input", "")
+    opt1 = st.text_input("opt1", "")
+    opt2 = st.text_input("opt2", "")
+    # opt3 = st.text_input("opt3", "")
+    # opt4 = st.text_input("opt4", "")
+
     # importing dataset for test
     uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
     if st.button("Predict"):
-        if (uploaded_file is not None and opt1 != ""):
+        # Validation, checking if user populated input fields and uploaded dataset
+        if (uploaded_file is not None and (opt1 != "" and opt2 != "")):
             st.text("Please choose only one option")
-        elif (uploaded_file is not None):
-            result = predict_note_authentication(uploaded_file)
-            with open('data.csv', mode='a', newline='') as file:
-                writer = csv.writer(file)
-            with st.expander("See Predictions"):
-                for i, item in enumerate(result):
-                    st.write(i+1, " {}".format(round(item, 2)))
-                    save_data_to_csv(item)
+        elif uploaded_file is not None:
+            # if a file was uploaded, run the model, output to a csv file
+            result = predict_danger_csv(uploaded_file)
+            for i, item in enumerate(result):
+                save_data_to_csv(item)
+            # with st.expander("See Predictions"):
+            #     for i, item in enumerate(result):
+            #         st.write(i+1, " {}".format(round(item, 2)))
+            #         save_data_to_csv(item)
             st.success("Succss")
             flag = 1
         else:
-            result = predict_note_authentication(opt1)
-
-    # if st.button("About"):
-    #     st.text("Lets Learn")
-    #     st.text("Built with Streamlit")
-    # if st.button("Download CSV"):
-    #     download_csv_file()
+            result = predict_danger_single([int(opt1), int(opt2)])
+            st.write("Result is: {}".format(round(result[0])))
+            st.success("Succss")
 
     if flag == 1:
         with open('data.csv', 'r') as file:
